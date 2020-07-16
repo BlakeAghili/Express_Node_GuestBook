6282799f0eb6f2d93dcabf99684a4987dcbbc665
Blake Aghili <peopleofksmo@gmail.com>
2020-07-16 17:08:08 -0500

    App.js ... Now on to the Views - EJS

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var createError = require('http-errors');
+var http = require('http');
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser')
@@ -6,6 +6,46 @@ var logger = require('morgan');
 
 var app = express();
 
+app.use(logger("dev"));
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+var entries = [];
+app.locals.entries = entries;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function (request, response) {
+    response.render("index");
+});
+
+app.get("/new-entry", function (request, response) {
+    response.render("new-entry")
+})
+
+// now a HTTP POST
+app.post("/new-entry", function (request, response) {
+    if(!request.body.title || !request.body.title) {
+        response.status(400).send("Entry is missing a title and a body");
+        return;
+    }
+
+    entries.push({
+        title: request.body.title,
+        body: request.body.body,
+        published: new Date()
+    });
+
+    response.redirect("/");
+});
+
+// if we are here, bad
+app.use(function(request, response) {
+    response.status(404).render("404");
+})
+
+http.createServer(app).listen(3000, function () {
+    console.log("GuestBook app started on port 3000");
+});
